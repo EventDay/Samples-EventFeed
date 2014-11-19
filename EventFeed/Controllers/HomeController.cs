edc55f1f9f025152a77b7444ffab343d27336194
Chris Martin <chris.martin@eventday.com>
2014-11-19 15:22:25 -0700

    Initial commit

---
+++ EventFeed/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Linq;
+using EventDay.Api.Client;
+
+namespace EventFeed.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEventDayApiClient _apiClient;
+
+        public HomeController(IEventDayApiClient apiClient)
+        {
+            _apiClient = apiClient;
+        }
+
+        // GET: Home
+        public async Task<ActionResult> Index()
+        {
+            EventSummaryState[] states = await _apiClient.GetEventSummariesAsync();
+            
+            var feed = new SyndicationFeed("My Company", "All Upcoming Events", null);
+            feed.Authors.Add(new SyndicationPerson("events@mycompany.com", "Events", "http://events.mycompany.com"));
+
+            var items = new List<SyndicationItem>();
+
+            foreach (var @event in states)
+            {
+                var item = new SyndicationItem(@event.Title, @event.Description, @event.ShortUrl)
+                {
+                    PublishDate = DateTime.UtcNow
+                };
+
+                item.ElementExtensions.Add(new XElement("startDate", @event.StartDateUtc.ToString("O")).CreateReader());
+                item.ElementExtensions.Add(new XElement("endDate", @event.EndDateUtc.ToString("O")).CreateReader());
+                item.ElementExtensions.Add(new XElement("city", @event.City).CreateReader());
+                item.ElementExtensions.Add(new XElement("state", @event.State).CreateReader());
+                
+                items.Add(item);
+            }
+
+            feed.Items = items;
+
+            Encoding encoding = Encoding.UTF8;
+
+            using(var writer = new StringWriter())
+            using (var xml = new XmlTextWriter(writer))
+            {
+                feed.SaveAsRss20(xml);
+                string rss = writer.ToString();
+                return Content(rss, "application/rss+xml", encoding);
+            }
+
+        }
+    }
+}
---
+++ EventFeed/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using EventDay.Api.Client.Configuration;
+using EventDay.Api.Client.Mvc.Autofac;
+using EventFeed.Controllers;
+
+namespace EventFeed
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            IEventDayApiConfiguration configuration = ApiConfiguration.FromConfig();
+
+            EventDayConfiguration.ForMvc<HomeController>()
+                .Configure(configuration);
+        }
+    }
+}
